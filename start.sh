c3978253efcbae1f0391ffdf96a9e8cf48631dac
yeahwu <yeahwu@users.noreply.github.com>
2020-09-29 19:36:31 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # run ss
-ss-server -s 0.0.0.0 -p ${PORT} -k ${PASSWORD} -m $METHOD --plugin /usr/bin/v2ray-plugin_linux_amd64 --plugin-opts "server;path=${WSPATH}"
+ss-server -s 0.0.0.0 -p ${PORT} -k ${PASSWORD} -m chacha20-ietf-poly1305 --plugin /usr/bin/v2ray-plugin_linux_amd64 --plugin-opts "server;path=${WSPATH}"
